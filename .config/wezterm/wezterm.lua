b2b9cc3856b4fe05f9aa026758fa193f12b9fdd5
Jaco Minnaar <jminnaar@bscglobal.com>
2024-02-19 16:10:58 +0200

    update wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -12,14 +12,25 @@ if wezterm.config_builder then config = wezterm.config_builder() end
 --
 
 -- Set the font size to 12pt
-config.font = wezterm.font("JetBrains Mono")
-config.font_size = 10
-config.dpi = 109.0
+config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.font_size = 11
 
 -- For example, changing the color scheme:
 config.color_scheme = 'GruvboxDark'
 
 config.hide_tab_bar_if_only_one_tab = true
+config.enable_wayland = true
+config.window_padding = {
+    left = 3,
+    right = 3,
+    top = 3,
+    bottom = 3,
+}
+
+-- config.window_background_image = "/home/jaco/.config/hypr/wallpaper.jpg"
+-- config.window_background_image_hsb = {
+--     brightness = 0.3,
+-- }
 
 -- and finally, return the configuration to wezterm
 return config
